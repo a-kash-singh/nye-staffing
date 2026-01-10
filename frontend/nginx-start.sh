596f0a09fd9ec8d31fd264e8ba255473aa412e0c
Akash Singh <akashsingh@Akashs-MacBook-Pro-2.local>
2026-01-10 23:21:45 +0530

    Update nginx startup script for better Render compatibility

--- frontend/nginx-start.sh
+++ frontend/nginx-start.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # Replace BACKEND_URL placeholder in nginx config
-BACKEND_URL=${BACKEND_URL:-http://backend:3000}
+# Default to localhost for docker-compose, or use env var for Render
+BACKEND_URL=${BACKEND_URL:-http://localhost:3000}
 
-# Use sed to replace the placeholder
-sed -i "s|\${BACKEND_URL}|${BACKEND_URL}|g" /etc/nginx/conf.d/default.conf
+# Use sed to replace the placeholder in the template
+sed "s|\${BACKEND_URL}|${BACKEND_URL}|g" /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 exec nginx -g 'daemon off;'
